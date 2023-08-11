7f483bd4c5bea5e32c78b9a44fb6ff3320cd33d7
KarunaJalgaonkar <karuna@enertect.com>
2023-08-11 12:36:54 +0530

    1

--- v7sql.js
+++ v7sql.js
@@ -125,43 +125,64 @@ async function voltageSaveDBSQL(value,firstBatteryId)
 {
     for (i=0, j=firstBatteryId; i<value.length; i++, j++) {
      
-      let batteryIdinsert=j;
-      let Value=value[i];
+       //*********************************Add in DB*****************************************
+       var myHeaders = new Headers();
+       myHeaders.append("Content-Type", "application/json");
+
+       var raw = JSON.stringify({
+           "BatteryId": j,
+         "Value": parseInt(value[i])/1000
+       });
+
+       var requestOptions = {
+         method: 'POST',
+         headers: myHeaders,
+         body: raw,
+         redirect: 'follow'
+       };
+
+       fetch("http://localhost:1212/insertInDashboardVoltage", requestOptions)
+         .then(response => response.text())
+         .then(result => console.log(result))
+         .catch(error => console.log('error', error));
+       //********************************************************************************************
+      // let batteryIdinsert=j;
+      // let Value=value[i];
     
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        var raw = JSON.stringify({ "BatteryId": batteryIdinsert });
-        var requestOptions = { method: 'POST', headers: myHeaders, body: raw, redirect: 'follow' };
+      //   var myHeaders = new Headers();
+      //   myHeaders.append("Content-Type", "application/json");
+      //   var raw = JSON.stringify({ "BatteryId": batteryIdinsert });
+      //   var requestOptions = { method: 'POST', headers: myHeaders, body: raw, redirect: 'follow' };
 
-        fetch("http://localhost:1212/checkDashboardVoltageByBatteryID", requestOptions).then(response => response.text())
-          .then(result =>{
-            var tempJSON = JSON.parse(result);
-             var count=tempJSON.recordset[0].Count;        
+      //   fetch("http://localhost:1212/checkDashboardVoltageByBatteryID", requestOptions).then(response => response.text())
+      //     .then(result =>{
+      //       var tempJSON = JSON.parse(result);
+      //        var count=tempJSON.recordset[0].Count;        
                      
-            if (count == 0)
-            {
-               console.log("Insert--"+"count : " + count + "--batteryidinsert : " + batteryIdinsert+ "--Value : "+Value)
-               var myHeaders = new Headers();
-               myHeaders.append("Content-Type", "application/json");
-                var raw = JSON.stringify({"BatteryId": batteryIdinsert,"Value":Value/1000 });
-                var requestOptions = { method: 'POST',headers: myHeaders, body: raw, redirect: 'follow'};
+      //       if (count == 0)
+      //       {
+      //          console.log("Insert--"+"count : " + count + "--batteryidinsert : " + batteryIdinsert+ "--Value : "+Value)
+      //          var myHeaders = new Headers();
+      //          myHeaders.append("Content-Type", "application/json");
+      //           var raw = JSON.stringify({"BatteryId": batteryIdinsert,"Value":Value/1000 });
+      //           var requestOptions = { method: 'POST',headers: myHeaders, body: raw, redirect: 'follow'};
 
-              fetch("http://localhost:1212/insertInDashboardVoltage", requestOptions).then(response => response.text())
-                .then(result => console.log(result))
-                .catch(error => console.log('error', error));
-           }
-            else
-            {
+      //         fetch("http://localhost:1212/insertInDashboardVoltage", requestOptions).then(response => response.text())
+      //           .then(result => console.log(result))
+      //           .catch(error => console.log('error', error));
+      //      }
+      //       else
+      //       {
             
-             var myHeaders = new Headers();
-             myHeaders.append("Content-Type", "application/json");
-             var raw = JSON.stringify({ "Value":Value/1000 ,"BatteryId": batteryIdinsert });
-             var requestOptions = { method: 'PUT', headers: myHeaders, body: raw, redirect: 'follow' };
+      //        var myHeaders = new Headers();
+      //        myHeaders.append("Content-Type", "application/json");
+      //        var raw = JSON.stringify({ "Value":Value/1000 ,"BatteryId": batteryIdinsert });
+      //        var requestOptions = { method: 'PUT', headers: myHeaders, body: raw, redirect: 'follow' };
 
-            fetch("http://localhost:1212/updateDashboardVoltageByBatteryID", requestOptions).then(response => response.text())
-              .then(result => console.log(result))
-              .catch(error => console.log('error', error)); }
-       }) .catch(error => console.log('error', error));
+      //       fetch("http://localhost:1212/updateDashboardVoltageByBatteryID", requestOptions).then(response => response.text())
+      //         .then(result => console.log(result))
+      //         .catch(error => console.log('error', error)); }
+      //  }) .catch(error => console.log('error', error));
        
   }
 }
